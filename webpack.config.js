8f284dfa9ea82e4c0f4f1b26a803fbede98f1c39
rommelvisuals <rommelmsalvatierra@gmail.com>
2024-02-10 13:32:24 +0800

    Fixed code spacing

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/script.js',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+    entry: './js/script.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }, ],
+    },
 };
